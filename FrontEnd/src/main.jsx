8bb40a005a5b7ccbc225c1a0365c6d704fd2e390
GitanshuHatwar <gitanshuhatwar@gmail.com>
2025-09-20 00:40:08 +0530

    Added Chat , Home , About , Contact and login page

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
-import JalSathiChat from './pages/JalSathiChat.jsx'
+import Navigation from './components/Navigation'
+import Home from './pages/Home'
+import JalSathiChat from './pages/JalSathiChat'
+import About from './pages/About'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<JalSathiChat />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <JalSathiChat />
+    <App />
   </StrictMode>,
 )
